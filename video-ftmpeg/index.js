675a6a623c9263311a54fa5f466af4d29ac99e01
Lucas Ertel <lucassoares@alunos.utfpr.edu.br>
2022-11-25 21:29:37 -0300

    Alteração e inclusão do fftmpeg , do fs gerenciar arquivos e remover

--- video-ftmpeg/index.js
+++ video-ftmpeg/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const multer = require("multer");
+const fs = require("fs");
+const ffmepg = require("fluent-ffmpeg");
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+
+ffmepg.setFfmpegPath(ffmpegPath);
 
 const PORT = process.env.Port || 5000;
 const app =  express();
@@ -12,10 +17,34 @@ const storage = multer.diskStorage({
          callback(null, Date.now() + '-' + file.originalname);
     },
 });
-
+console.log(ffmepg);
 app.use(multer({storage}).single("file"));
 app.post('/convert',(request ,response) => {
-    return response.send("ok");
+    const file = request.file;
+    const fileName = ' output.mp3';
+
+    ffmepg('tmp/' + file.filename).toFormat('mp3').
+    on("end",() =>{
+        //Fazer download do arquivo mp3
+        return response.download(__dirname + fileName, (error) =>{
+            if (error) throw  error;
+            console.log("convertion sucess");
+
+        });
+
+        // Apagar arquivo mp4
+    }).on("error" , (error) =>{
+        console.log(error);
+        //Apagar arquivo mp4
+    }).saveToFile(__dirname + fileName)
+
 });
+
+function removeFile(directory){
+  fs.unlink(directory,(error) =>{
+      if (error) throw  error;
+      console.log("File deleted");
+  })
+}
 
 app.listen(PORT);
